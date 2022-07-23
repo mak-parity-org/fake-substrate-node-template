974621f8929f96592275df8e907f1fc6fa508637
Maksym Hlukhovtsov <mordamax@gmail.com>
2022-07-23 16:58:24 +0100

    polkadot0.9.25

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -249,7 +249,6 @@ impl pallet_balances::Config for Runtime {
 }
 
 impl pallet_transaction_payment::Config for Runtime {
-	type Event = Event;
 	type OnChargeTransaction = CurrencyAdapter<Balances, ()>;
 	type OperationalFeeMultiplier = ConstU8<5>;
 	type WeightToFee = IdentityFee<Balance>;
